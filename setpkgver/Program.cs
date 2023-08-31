73626b9e3215facdab8bf7697ac78b5835da660c
LTRData <ol@ltr-data.se>
2023-08-31 21:55:20 +0200

    Syntax changes etc

--- setpkgver/Program.cs
+++ setpkgver/Program.cs
@@ -1,4 +1,5 @@
-﻿using LTRLib.LTRGeneric;
+﻿using LTRLib.Extensions;
+using LTRLib.LTRGeneric;
 using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Protocol;
@@ -6,6 +7,7 @@ using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -41,8 +43,8 @@ public static class Program
 
         var setExplicit = false;
         var upgradeOnly = false;
-        var execute = false;
-        var listAll = false;
+        var saveChanges = false;
+        var verbose = false;
         var recursive = false;
         Regex[] includeFilters = Array.Empty<Regex>();
         Regex[] excludeFilters = Array.Empty<Regex>();
@@ -53,6 +55,7 @@ public static class Program
             {
                 // Only change * versions to currently latest version explicitly
                 case "explicit":
+                case "e":
                     Debug.Assert(cmd.Value.Length == 0, "--explicit option cannot have values");
                     Debug.Assert(!upgradeOnly, "Cannot set both --upgrade and --explicit");
                     setExplicit = true;
@@ -60,35 +63,41 @@ public static class Program
 
                     // Only upgrade packages with an explicit version number set
                 case "upgrade":
+                case "u":
                     Debug.Assert(cmd.Value.Length == 0, "--upgrade option cannot have values");
                     Debug.Assert(!setExplicit, "Cannot set both --upgrade and --explicit");
                     upgradeOnly = true;
                     break;
 
                     // Save changes
-                case "exec":
-                    Debug.Assert(cmd.Value.Length == 0, "--exec option cannot have values");
-                    execute = true;
+                case "save":
+                case "s":
+                    Debug.Assert(cmd.Value.Length == 0, "--save option cannot have values");
+                    saveChanges = true;
                     break;
 
                 // Regex package name filter
                 case "include":
+                case "i":
                     Debug.Assert(cmd.Value.Length >= 1, "--include= option requires regex values");
                     includeFilters = Array.ConvertAll(cmd.Value, val => new Regex(val, RegexOptions.IgnoreCase | RegexOptions.Compiled));
                     break;
 
                 case "exclude":
+                case "x":
                     Debug.Assert(cmd.Value.Length >= 1, "--exclude= option requires regex values");
                     excludeFilters = Array.ConvertAll(cmd.Value, val => new Regex(val, RegexOptions.IgnoreCase | RegexOptions.Compiled));
                     break;
 
                 // List all found package names
-                case "list":
-                    Debug.Assert(cmd.Value.Length == 0, "--list option cannot have values");
-                    listAll = true;
+                case "v":
+                case "verbose":
+                    Debug.Assert(cmd.Value.Length == 0, "--verbose option cannot have values");
+                    verbose = true;
                     break;
 
                 case "r":
+                case "recurse":
                     Debug.Assert(cmd.Value.Length == 0, "-r option cannot have values");
                     recursive = true;
                     break;
@@ -97,7 +106,32 @@ public static class Program
                     break;
 
                 default:
-                    throw new InvalidOperationException($"Unknown option: {cmd.Key}");
+                    Console.WriteLine(
+@"Utility to upgrade NuGet version references in .NET project files
+Copyright 2023 - Olof Lagerkvist, LTR Data - https://ltr-data.se
+
+Usage:
+setpkgver [-options] files ...
+
+--verbose           Shows each package reference found in project files with
+-v                  selected filters.
+
+--exclude=pattern   Exclude packages that match a regex pattern.
+-x=pattern
+
+--include=pattern   Include only packages that match a regex pattern.
+-i=pattern
+
+--save              Save changes to project files. Otherwise just dry-run.
+-s
+
+--upgrade           Upgrade versions for packages where there are newer
+-u                  versions available.
+
+--explicit          Change version references with asterisks to explicit
+                    version number available matching the existing pattern.");
+
+                    return -1;
             }
         }
 
@@ -109,7 +143,7 @@ public static class Program
         var repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
         var resource = await repository.GetResourceAsync<FindPackageByIdResource>().ConfigureAwait(false);
 
-        var packageVersions = new ConcurrentDictionary<string, NuGetVersion?>(StringComparer.Ordinal);
+        var packageVersions = new ConcurrentDictionary<string, Task<IReadOnlyList<NuGetVersion>>>(StringComparer.Ordinal);
 
         var files = cmds[""].AsEnumerable();
 
@@ -163,7 +197,7 @@ public static class Program
                 if (setExplicit)
                 {
                     packageReferences = packageReferences.
-                        Where(e => e.Attribute("Version")?.Value == "*");
+                        Where(e => e.Attribute("Version")?.Value?.EndsWith("*", StringComparison.Ordinal) ?? false);
                 }
 
                 if (upgradeOnly)
@@ -180,15 +214,51 @@ public static class Program
 
                     var version = node.Attribute("Version")?.Value;
 
-                    var latestVersion = packageVersions.GetOrAdd(packageName,
-                        packageName =>
+                    var versions = await packageVersions.GetOrAdd(packageName,
+                        async packageName =>
                         {
-                            var versions = resource.GetAllVersionsAsync(packageName, cache, NullLogger.Instance, CancellationToken.None).Result;
+                            if (verbose)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.WriteLine($"Checking latest version for {packageName}...");
+                                Console.ResetColor();
+                            }
 
-                            var latestVersion = versions.OrderByDescending(v => v.IsPrerelease).ThenBy(v => v).LastOrDefault();
+                            try
+                            {
+                                var versions = await resource.GetAllVersionsAsync(packageName, cache, NullLogger.Instance, CancellationToken.None).ConfigureAwait(false);
+                                return versions.ToList();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Error.WriteLine($"Error checking version for package '{packageName}': {ex.JoinMessages()}");
+                                Console.ResetColor();
+                                return Array.Empty<NuGetVersion>();
+                            }
+                        }).ConfigureAwait(false);
 
-                            return latestVersion;
-                        });
+                    NuGetVersion? latestVersion;
+                    
+                    if (version is not null
+                        && setExplicit
+                        && version.EndsWith("*", StringComparison.Ordinal))
+                    {
+                        var versionPrefix = version.TrimEnd('*');
+
+                        latestVersion = versions
+                            .Where(v => v.OriginalVersion?.StartsWith(versionPrefix, StringComparison.Ordinal) ?? false)
+                            .OrderByDescending(v => v.IsPrerelease)
+                            .ThenBy(v => v)
+                            .LastOrDefault();
+                    }
+                    else
+                    {
+                        latestVersion = versions
+                            .OrderByDescending(v => v.IsPrerelease)
+                            .ThenBy(v => v)
+                            .LastOrDefault();
+                    }
 
                     if (latestVersion is null)
                     {
@@ -217,7 +287,7 @@ public static class Program
 
                         modified = true;
                     }
-                    else if (listAll)
+                    else if (verbose)
                     {
                         Console.ForegroundColor = ConsoleColor.Gray;
                         Console.WriteLine($"{packageName}: {version}");
@@ -231,7 +301,7 @@ public static class Program
                     Console.WriteLine($"No changes to '{arg}'.");
                     Console.ResetColor();
                 }
-                else if (execute)
+                else if (saveChanges)
                 {
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine($"Saving '{arg}'...");
@@ -241,7 +311,7 @@ public static class Program
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine($"Changes to '{arg}' not saved. Run again with --exec to save chages.");
+                    Console.WriteLine($"Changes not saved. Run again with --save to save chages.");
                     Console.ResetColor();
                 }
             }
